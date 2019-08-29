56d57c013b0197d59b8a964092db3a0fa3a1101e
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2019-08-29 14:23:17 -0500

    working on new search feature

--- src_search/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Dropdown from './Dropdown';
-import './index.css';
-
-ReactDOM.render(
-    <Dropdown />,
-    document.getElementById('root')
-);
